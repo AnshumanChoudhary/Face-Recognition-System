27a3491bdeada5fa5d141358fd154a01efe62b9b
Anshuman Choudhary <103357511+AnshumanChoudhary@users.noreply.github.com>
2024-07-16 00:56:54 +0530

    Update Face recognition.py

--- Face recognition.py
+++ Face recognition.py
@@ -17,10 +17,10 @@ cv2.imshow("video_live",video_data)
 if cv2.waitKey(10) == ord("a"):
 break
 video_cap.release()
-#video_cap = cv2.VideoCapture(0)
-#while True :
-# ret , video_data = video_cap.read()
-# cv2.imshow("video_live",video_data)
-# if cv2.waitKey(10) == ord("a"):
-# break
-#video_cap.release()
+video_cap = cv2.VideoCapture(0)
+while True :
+ret , video_data = video_cap.read()
+cv2.imshow("video_live",video_data)
+if cv2.waitKey(10) == ord("a"):
+break
+video_cap.release()
